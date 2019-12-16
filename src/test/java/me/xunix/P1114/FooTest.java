44584eef7fd9d94337207f92a7d019b509849b78
xunix huang <qiwei_huang@qq.com>
2019-12-16 20:47:21 +0800

    modify pom jdk version

--- src/test/java/me/xunix/P1114/FooTest.java
+++ src/test/java/me/xunix/P1114/FooTest.java
@@ -4,51 +4,33 @@ public class FooTest {
   public static void main(String[] args) throws InterruptedException {
     final Foo foo = new Foo();
 
-    final Runnable printFirst = new Runnable() {
-      public void run() {
-        System.out.println("1 ");
-      }
-    };
+    final Runnable printFirst = () -> System.out.println("1 ");
 
-    final Runnable printSecond = new Runnable() {
-      public void run() {
-        System.out.println("2 ");
-      }
-    };
+    final Runnable printSecond = () -> System.out.println("2 ");
 
-    final Runnable printThird = new Runnable() {
-      public void run() {
-        System.out.println("3 ");
-      }
-    };
+    final Runnable printThird = () -> System.out.println("3 ");
 
-    Thread t1 = new Thread(new Runnable() {
-      public void run() {
-        try {
-          foo.first(printFirst);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
+    Thread t1 = new Thread(() -> {
+      try {
+        foo.first(printFirst);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
     });
 
-    Thread t2 = new Thread(new Runnable() {
-      public void run() {
-        try {
-          foo.second(printSecond);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
+    Thread t2 = new Thread(() -> {
+      try {
+        foo.second(printSecond);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
     });
 
-    Thread t3 = new Thread(new Runnable() {
-      public void run() {
-        try {
-          foo.third(printThird);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
+    Thread t3 = new Thread(() -> {
+      try {
+        foo.third(printThird);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
     });
 
--- src/test/java/me/xunix/P1115/FooBarTest.java
+++ src/test/java/me/xunix/P1115/FooBarTest.java
@@ -1,4 +1,51 @@
 package me.xunix.P1115;
 
 public class FooBarTest {
+  public static void main(String[] args) throws InterruptedException {
+
+    int n = 1000;
+    FooBar fooBar = new FooBar(n);
+
+    StringBuilder sb = new StringBuilder();
+
+    Runnable printFoo = () -> {
+      sb.append("foo");
+    };
+    Runnable printBar = () -> {
+      sb.append("bar");
+    };
+
+    Thread t1 = new Thread(() -> {
+      try {
+        fooBar.foo(printFoo);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+    Thread t2 = new Thread(() -> {
+      try {
+        fooBar.bar(printBar);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+    t1.start();
+    t2.start();
+
+    t1.join();
+    t2.join();
+
+    String result = sb.toString();
+    System.out.println(result);
+
+    String expected = "";
+    for (int i = 0; i < n; i++) {
+      expected = expected.concat("foobar");
+    }
+
+    if (!result.equals(expected)) {
+      throw new RuntimeException("the answer is not right");
+    }
+
+  }
 }
