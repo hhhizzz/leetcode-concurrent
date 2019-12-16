cb868bdeb6697b2d050947cd99f72eeb162d48aa
xunix huang <qiwei_huang@qq.com>
2019-12-16 21:30:24 +0800

    add 1116

---
+++ src/main/java/me/xunix/P1116/ZeroEvenOdd.java
@@ -0,0 +1,63 @@
+package me.xunix.P1116;
+
+interface IntConsumer {
+  void accept(int x);
+}
+
+class ZeroEvenOdd {
+  private int n;
+  private int current = 1;
+  private boolean printZero = true;
+
+  public ZeroEvenOdd(int n) {
+    this.n = n;
+  }
+
+  // printNumber.accept(x) outputs "x", where x is an integer.
+  public void zero(IntConsumer printNumber) throws InterruptedException {
+    for (int i = 0; i < n; i++) {
+      synchronized (this) {
+        while (!printZero) {
+          this.wait();
+        }
+        printNumber.accept(0);
+        this.printZero = false;
+        this.notifyAll();
+      }
+    }
+  }
+
+  public void even(IntConsumer printNumber) throws InterruptedException {
+    int half = n / 2;
+    for (int i = 1; i <= half; i++) {
+      synchronized (this) {
+        while (printZero || this.current % 2 != 0) {
+          this.wait();
+        }
+        printNumber.accept(current);
+        this.current += 1;
+        this.printZero = true;
+        this.notifyAll();
+      }
+    }
+
+  }
+
+  public void odd(IntConsumer printNumber) throws InterruptedException {
+    int half = n / 2;
+    if (n % 2 == 1) {
+      half += 1;
+    }
+    for (int i = 1; i <= half; i++) {
+      synchronized (this) {
+        while (printZero || this.current % 2 != 1) {
+          this.wait();
+        }
+        printNumber.accept(current);
+        this.current += 1;
+        this.printZero = true;
+        this.notifyAll();
+      }
+    }
+  }
+}
---
+++ src/test/java/me/xunix/P1116/ZeroEvenOddTest.java
@@ -0,0 +1,61 @@
+package me.xunix.P1116;
+
+public class ZeroEvenOddTest {
+  public static void main(String[] args) throws InterruptedException {
+    int n = 5;
+    ZeroEvenOdd zeroEvenOdd = new ZeroEvenOdd(n);
+    StringBuilder builder = new StringBuilder();
+    StringBuilder finalBuilder = builder;
+    IntConsumer consumer = x -> {
+      System.out.print(x);
+      finalBuilder.append(x);
+    };
+
+    Thread tZero = new Thread(() -> {
+      try {
+        zeroEvenOdd.zero(consumer);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+    Thread tOdd = new Thread(() -> {
+      try {
+        zeroEvenOdd.odd(consumer);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+    Thread tEven = new Thread(() -> {
+      try {
+
+        zeroEvenOdd.even(consumer);
+
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+
+    tZero.start();
+    tEven.start();
+    tOdd.start();
+
+    tZero.join();
+    tEven.join();
+    tOdd.join();
+
+
+    String actual = builder.toString();
+    System.out.println();
+
+    builder = new StringBuilder();
+    for (int i = 1; i <= n; i++) {
+      builder.append(0);
+      builder.append(i);
+    }
+    String expect = builder.toString();
+    System.out.println(expect);
+    if (!actual.equals(expect)) {
+      throw new RuntimeException("the answer is not right");
+    }
+  }
+}
